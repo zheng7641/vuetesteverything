f493ae7235bd0b948e97947bbeb8329e5a8e0c2c
zhengchentong <zhengchentong@xdf.cn>
2021-01-27 20:41:24 +0800

    布局

--- src/pages/globle.vue
+++ src/pages/globle.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-    <div id="app">
-      <el-button @click="visible = true">Button</el-button>
-      <el-dialog :visible.sync="visible" title="Hello world">
-        <p>Try Element</p>
-      </el-dialog>
-    </div>
-  </div>
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        overflow-x:hidden
+      >
+        <el-menu-item index="1" >处理中心</el-menu-item>
+        <el-menu-item index="2">处理中心</el-menu-item>
+        <el-menu-item index="3">处理中心</el-menu-item>
+        
+      </el-menu>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
    
 <script>
@@ -14,3 +32,40 @@ export default {
   name: "globle",
 };
 </script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: calc(100vh - 180px);
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
